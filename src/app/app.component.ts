84ca8e7a54df75de84101d25e5223a4256631706
rohit.patil <rohit.patil@anchanto.com>
2020-08-01 23:03:13 +0530

    title name changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-rest-apis-project';
+  title = 'Welcome to github-rest-apis-project';
 }
